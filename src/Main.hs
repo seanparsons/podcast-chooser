99051382ccaa34007ae32b10ff087ad61888b45d
Sean Parsons <git@futurenotfound.com>
2017-07-26 00:22:12 +0100

    Fully functional chooser.

--- src/Main.hs
+++ src/Main.hs
@@ -3,32 +3,32 @@
 import Data.Aeson
 import Data.ByteString.Lazy (fromStrict)
 import Data.Foldable
-import Data.Text hiding (lines)
+import Data.Maybe
+import Data.Text hiding (lines, find, filter)
 import Data.Text.Encoding
-import Turtle hiding (text)
+import Turtle hiding (text, find)
 import qualified Control.Foldl as F
 import Data.Aeson.Lens
-import Control.Lens
+import Control.Lens hiding (Choice)
 import Data.List
 import System.Console.Byline
 import System.Console.Byline.Menu
 
-
 data PodcastShow = PodcastShow
-                 { podcastFile   :: Text
-                 , podcastTitle  :: Maybe Text
-                 , feedTitle     :: Maybe Text
+                 { feedTitle  :: Maybe Text
+                 , showTitle  :: Maybe Text
+                 , showFile   :: Text
                  } deriving (Eq, Show)
 
 feedTitleLens :: Lens' PodcastShow (Maybe Text)
-feedTitleLens = lens feedTitle (\s -> \a -> s { feedTitle = a})
+feedTitleLens = lens feedTitle (\s -> \a -> s {feedTitle = a})
 
 parsePodcastShow :: Text -> Either String PodcastShow
 parsePodcastShow value = maybe (Left ("Can't parse podcast: " ++ (unpack value))) return $ do
   pFile <- value ^? key "file" . _String
-  let pTitle = value ^? key "fields" . key "title" . nth 0 . _String
+  let sTitle = value ^? key "fields" . key "title" . nth 0 . _String
   let fTitle = value ^? key "fields" . key "feedtitle" . nth 0 . _String
-  return $ PodcastShow pFile pTitle fTitle
+  return $ PodcastShow fTitle sTitle pFile
 
 parseValue :: Text -> IO [PodcastShow]
 parseValue value =
@@ -46,17 +46,27 @@ runGitAnnex = foldIO gitAnnexShell (F.premapM lineToText $ F.sink parseValue)
 uniqueFeedTitles :: [PodcastShow] -> [Text]
 uniqueFeedTitles = nub . toListOf (traverse . feedTitleLens . _Just)
 
+choiceToMaybe :: Choice a -> Maybe a
+choiceToMaybe (Match matched) = Just matched
+choiceToMaybe _               = Nothing
+
 choosePodcast :: [PodcastShow] -> Byline IO (Maybe PodcastShow)
 choosePodcast podcastShows = do
   let feedTitles = uniqueFeedTitles podcastShows
   let feedMenu = menu feedTitles text
-  feedChoice <- askWithMenu feedMenu (text "Choose Feed")
-  liftIO $ print feedChoice
-  return Nothing
+  feedChoice <- fmap choiceToMaybe $ askWithMenu feedMenu (text "Choose Feed")
+  let feedShows = filter (\s -> feedTitle s == feedChoice) podcastShows
+  let showMenu = menu (feedShows >>= (maybeToList . showTitle)) text
+  showChoice <- fmap choiceToMaybe $ askWithMenu showMenu (text "Choose Show")
+  let podcastShow = find (\s -> feedTitle s == feedChoice && showTitle s == showChoice) podcastShows
+  return podcastShow
 
 main :: IO ()
 main = do
   podcastShows <- runGitAnnex
   print $ uniqueFeedTitles podcastShows
-  podcastChosen <- runByline $ choosePodcast podcastShows
-  print podcastChosen
+  podcastChosen <- fmap join $ runByline $ choosePodcast podcastShows
+  let podcastFile = fmap showFile podcastChosen
+  let noShowChosen = print "No Show Chosen."
+  let playPodcast f = procs "vlc" [f] mempty
+  maybe noShowChosen playPodcast podcastFile
